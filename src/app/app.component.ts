f0b8e1031b536f7aff713aaf649ba31224c9c00f
Gianluca Zanzarella <gianlucazanza@hotmail.com>
2024-12-06 22:57:53 +0100

    Installed Material Tailwind and NgRx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

e1f3d2734126997003b55545a0ceb8bf41bd2bfd
Gianluca Zanzarella <gianlucazanza@hotmail.com>
2024-12-08 19:34:54 +0100

    Moved stats state in store state

--- src/app/modules/store/pages/stats/stats.component.ts
+++ src/app/modules/store/pages/stats/stats.component.ts
@@ -1,15 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { IStats } from '../../interfaces/stats.interface';
+import { IStoreData } from '../../interfaces/store.interface';
 import { StoresActions } from '../../state/actions/store.action';
+import { StoreSelector } from '../../state/selectors/store.selector';
 
 @Component({
   selector: 'app-stats',
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './stats.component.html',
   styleUrl: './stats.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StatsComponent implements OnInit {
+
+  stats$: Observable<IStats[]> = this.store.select(StoreSelector.getStats);
+  statsLoading$: Observable<boolean> = this.store.select(StoreSelector.getStatsLoading);
+  store$: Observable<IStoreData | null> = this.store.select(StoreSelector.getStore);
+  storeLoading$: Observable<boolean> = this.store.select(StoreSelector.getStoreLoading);
 
   storeId: string | null = this.route.snapshot.paramMap.get('storeId');
 
